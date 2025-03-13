fc4f8663c2ecfebe1915fb8c6a170b54ff943d25
ashishratz007 <ashish@jogdigitalinnovations.com>
2025-03-13 13:21:35 +0530

    Upload success added

--- src/main/java/com/jogdesktopapp/Jog_Desktop_App/ApiCalls.java
+++ src/main/java/com/jogdesktopapp/Jog_Desktop_App/ApiCalls.java
@@ -17,6 +17,7 @@ public class ApiCalls {
             conn.setRequestProperty("Content-Type", "application/json; utf-8");
             conn.setRequestProperty("Accept", "application/json");
             conn.setDoOutput(true);
+            
 
             try (OutputStream os = conn.getOutputStream()) {
                 byte[] input = jsonInputString.getBytes("utf-8");
@@ -25,11 +26,14 @@ public class ApiCalls {
 
             int responseCode = conn.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
+            	System.out.println("Path setup sucess");
                 return "Success: " + responseCode;
             } else {
+            	System.out.println("Path setup Error");
                 return "Failed: " + responseCode;
             }
         } catch (Exception e) {
+        	System.out.println("Path setup Error");
             return "Error: " + e.getMessage();
         }
     }
