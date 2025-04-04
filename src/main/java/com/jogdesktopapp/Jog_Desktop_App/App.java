19be6914a6ceec93b2ec16dfc46d0a4ce585c541
ashishratz007 <ashish@jogdigitalinnovations.com>
2025-04-04 11:40:03 +0530

    added timmer to pending files

--- src/main/java/com/jogdesktopapp/Jog_Desktop_App/App.java
+++ src/main/java/com/jogdesktopapp/Jog_Desktop_App/App.java
@@ -1,10 +1,8 @@
 package com.jogdesktopapp.Jog_Desktop_App;
-
 import java.awt.*;
-import java.net.Socket;
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.handshake.ServerHandshake;
-
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import com.jogdesktopapp.Jog_Desktop_App.models.WindowsDeviceInfo;
 import javax.swing.SwingWorker;
 
@@ -15,8 +13,10 @@ public class App {
 	static SftpUploader sftpClient = SftpUploader.getInstance(); // actions client for upload download and many more
 	// to make work easier to filter you data
 	static GlobalDataClass globalData = GlobalDataClass.getInstance();
+	static WindowsDeviceInfo deviceInfo; 
+	
 	public static void main(String[] args) {
-		PermissionRequest.main(args);
+//		PermissionRequest.main(args);
 		EventQueue.invokeLater(() -> {
 			try {  
 //                synologyServer.init(); // Now it won't be null
@@ -26,18 +26,36 @@ public class App {
 					new SwingWorker<Void, Void>() {
 						@Override
 						protected Void doInBackground() throws Exception {
-							sftpClient.getPendingFiles(); // Runs in background
-							WindowsDeviceInfo deviceInfo = WindowsDeviceInfo.getInstance();
+							
+							
+						
+							/// to schedule the task for the that will execute after 15 seconds to check for pending files
+							// Create a scheduled executor
+					        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+					        // Schedule the task to run every 15 seconds with no initial delay
+					        scheduler.scheduleAtFixedRate(() -> {
+					        	
+					            /// Only call the pending API when the the uploading is not occurring
+					        	SftpUploaderStatus status = sftpClient.currentStatus;
+					        	if(status == SftpUploaderStatus.IDLE) {
+					        		 System.out.println("Status IDLE Function called at: " + java.time.LocalTime.now());
+					        		sftpClient.getPendingFiles(); // Runs in background
+					        	}
+					            
+					        }, 0, 60, TimeUnit.SECONDS);
+
 					        
-					        System.out.println("===== Device Identification =====");
-					        System.out.println("Device ID: " + deviceInfo.getDeviceId());
-					        System.out.println("Device Name: " + deviceInfo.getDeviceName());
-					        System.out.println("Product ID: " + deviceInfo.getProductId());
-					        
-					        System.out.println("\n===== Device Specifications =====");
-					        deviceInfo.getDeviceSpecs().forEach((key, value) -> {
-					            System.out.printf("%-25s: %s%n", key, value);
-					        });
+//					    	deviceInfo = WindowsDeviceInfo.getInstance();	
+//					        System.out.println("===== Device Identification =====");
+//					        System.out.println("Device ID: " + deviceInfo.getDeviceId());
+//					        System.out.println("Device Name: " + deviceInfo.getDeviceName());
+//					        System.out.println("Product ID: " + deviceInfo.getProductId());
+//					        
+//					        System.out.println("\n===== Device Specifications =====");
+//					        deviceInfo.getDeviceSpecs().forEach((key, value) -> {
+//					            System.out.printf("%-25s: %s%n", key, value);
+//					        });
 							return null;
 						}
 						@Override
